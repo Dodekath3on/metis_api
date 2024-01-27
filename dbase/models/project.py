ca308e0daf8f7e12e18e7d0d625e798080386bba
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-01-27 12:12:36 -0700

    created project methods

--- dbase/models/project.py
+++ dbase/models/project.py
@@ -16,3 +16,20 @@ class Project(models.Model):
   
   def users(self):
     return list(set(self.user_set.all()))
+  
+  def hours(self):
+    tasks = self.task_set.all()
+    hours_billed = 0
+    for task in tasks:
+      hours_billed += task.estimated_time.days * 8
+    return hours_billed
+  
+  def remaining_budget(self):
+    remaining_budget = self.budget
+    tasks = self.task_set.all()
+    for task in tasks:
+      remaining_budget -= task.estimated_time.days * 8 * task.assigned_to.bill_rate
+    return remaining_budget
+  
+  def remaining_budget_percentage(self):
+    return f'{int((self.remaining_budget() / self.budget) * 100)}%'
