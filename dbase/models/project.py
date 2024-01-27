e429aa2437baaf242da769dce5afd6c35ba9ddef
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-01-26 19:31:11 -0700

    added helper methods to user and project models

--- dbase/models/project.py
+++ dbase/models/project.py
@@ -3,7 +3,10 @@ from .team import Team
 
 class Project(models.Model):
   name = models.CharField(max_length=100)
-  team = models.ManyToManyField(Team)
+  teams = models.ManyToManyField(Team)
 
   def __str__(self):
     return f"{self.name}"
+  
+  def users(self):
+    return list(set(self.user_set.all()))
--- dbase/models/user.py
+++ dbase/models/user.py
@@ -1,9 +1,19 @@
 from django.db import models
+from .project import Project
+from .team import Team
 
 class User(models.Model):
   name = models.CharField(max_length=100)
   username = models.CharField(max_length=100, unique=True)
   email = models.EmailField(max_length=100, unique=True)
+  project_set = models.ManyToManyField(Project, through='Task')
+  teams = models.ManyToManyField(Team)
 
   def __str__(self):
     return f"{self.username}"
+  
+  def projects(self):
+    return list(set(self.project_set.all()))
+  
+  def tasks(self):
+    return self.task_set.all()
